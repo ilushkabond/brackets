9d74676b9848f873515568f9505636ec294560e8
ilushkabond <ilushkabond@gmail.com>
2022-03-11 23:49:27 +0300

    feat: made solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,17 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
-}
+  if (str.length % 2 === 1) {
+    return false;
+  }
+
+  const config = Object.fromEntries(bracketsConfig);
+
+  const stack = [];
+  [...str].forEach((char) => {
+    if (char === config[stack[stack.length - 1]]) {
+      stack.pop();
+    } else {
+      stack.push(char);
+    }
+  });
+  return !stack.length;
+};
